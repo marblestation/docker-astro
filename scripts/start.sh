7e809fb851443349164b5b9a78d6598142f86971
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2016-10-18 16:32:35 +0200

    Bug fix: git config was not considering spaces in username

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ ! -z "$GITEMAIL" ]; then
-    git config --global user.email $GITEMAIL
+    git config --global user.email "$GITEMAIL"
 fi
 if [ ! -z "$GITNAME" ]; then
-    git config --global user.name $GITNAME
+    git config --global user.name "$GITNAME"
 fi
 git config --global push.default simple
 
